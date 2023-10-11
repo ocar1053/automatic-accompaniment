74798983f6feb34db1e310b410c0689c5992c73f
ocar1053 <ocar8951@gmail.com>
2023-10-12 01:44:56 +0800

    lstm

--- src/data_process/transition__chord_matrix/transition_chord.py
+++ src/data_process/transition__chord_matrix/transition_chord.py
@@ -7,6 +7,7 @@ relative_path = os.path.dirname(os.path.realpath(__file__))
 
 
 def get_chord_file(relative_path) -> list:
+    print(relative_path)
     """
     get the chord file path list
 
@@ -27,67 +28,11 @@ def get_chord_file(relative_path) -> list:
 
     for file in file_list:
 
-        chord_file = os.path.join(parent_path, 'POP909', file, 'chord_midi.txt')
+        chord_file = os.path.join(
+            parent_path, 'POP909', file, 'chord_midi.txt')
         chord_file_list.append(chord_file)
 
     return chord_file_list
-
-
-def data_preprocess(chord_df) -> pd.DataFrame:
-    """
-    data preprocess to remove empty chord and add start_chord and end_chord
-    :param chord_df: the chord DataFrame
-    :return: chord_df
-    """
-
-    # remove the empty chord
-    chord_df = chord_df[chord_df['chord'] != 'N']
-
-    start_chord = pd.Series({'start_time': 0, 'end_time': chord_df['start_time'].iloc[0], 'chord': 'start_chord'})
-    end_chord = pd.Series({'start_time': chord_df['end_time'].iloc[-1], 'end_time': chord_df['end_time'].iloc[-1]+1, 'chord': 'end_chord'})
-
-    chord_df = pd.concat([pd.DataFrame(start_chord).T, chord_df], ignore_index=True)
-
-    chord_df = pd.concat([chord_df, pd.DataFrame(end_chord).T], ignore_index=True)
-
-    # simplfy the chord by change remove chord after '/' and remove chord after '(')
-    chord_df['chord'] = chord_df['chord'].apply(lambda x: x.split('/')[0])
-    chord_df['chord'] = chord_df['chord'].apply(lambda x: x.split('(')[0])
-    return chord_df
-
-
-def merge_all_df(file_list) -> pd.DataFrame:
-    """
-    merge all chord_df
-    :param file_list: the chord file list
-    :return: all_df
-    """
-
-    all_df = pd.DataFrame()
-
-    for index, file in enumerate(file_list):
-
-        chord_df = pd.read_csv(file, sep='\t', header=None, names=['start_time', 'end_time', 'chord'])
-
-        chord_df = data_preprocess(chord_df)
-        chord_df['song_num'] = index
-
-        all_df = pd.concat([all_df, chord_df])
-
-    # filter chords apeared less than 10 times
-    chord_count = all_df['chord'].value_counts()
-    chord_count = chord_count[chord_count <= 30]
-
-    # get chord name
-    chord_name = chord_count.index.to_list()
-
-    # filter chord
-    all_df = all_df[~all_df['chord'].isin(chord_name)]
-    all_df = all_df.reset_index(drop=True)
-
-    file_path = os.path.join(relative_path, 'csv_file\\all_chord.csv')
-    all_df.to_csv(file_path, index=True, header=True)
-    return all_df
 
 
 def get_transition_chord_normalize_each_time(all_df) -> pd.DataFrame:
@@ -107,19 +52,22 @@ def get_transition_chord_normalize_each_time(all_df) -> pd.DataFrame:
     chord_list = all_df['chord'].unique()
     chord_list.sort()
 
-    transitions = pd.DataFrame(np.zeros((len(chord_list), len(chord_list))), index=chord_list, columns=chord_list)
+    transitions = pd.DataFrame(np.zeros(
+        (len(chord_list), len(chord_list))), index=chord_list, columns=chord_list)
 
     # create transition matrix
     for i in range(len(all_df)-1):
         scale_value = all_df['song_num'].iloc[i]
-        transitions.loc[all_df['chord'].iloc[i], all_df['chord'].iloc[i+1]] += 1/chord_amount_each_song[scale_value]
+        transitions.loc[all_df['chord'].iloc[i], all_df['chord'].iloc[i+1]
+                        ] += 1/chord_amount_each_song[scale_value]
 
     transitions = transitions.apply(lambda x: x/x.sum(), axis=1)
 
     transitions = transitions.applymap(lambda x: '{:.2%}'.format(x))
 
     # save as csv
-    file_path = os.path.join(relative_path, 'csv_file\\transition_chord_normalize_each_time.csv')
+    file_path = os.path.join(
+        relative_path, 'csv_file\\transition_chord_normalize_each_time.csv')
     transitions.to_csv(file_path, index=True, header=True)
 
     return transitions
@@ -135,11 +83,13 @@ def get_transition_chord(all_df) -> pd.DataFrame:
     chord_list = all_df['chord'].unique()
     chord_list.sort()
 
-    transitions = pd.DataFrame(np.zeros((len(chord_list), len(chord_list))), index=chord_list, columns=chord_list)
+    transitions = pd.DataFrame(np.zeros(
+        (len(chord_list), len(chord_list))), index=chord_list, columns=chord_list)
 
     # get the transition matrix
     for i in range(len(all_df)-1):
-        transitions.loc[all_df['chord'].iloc[i], all_df['chord'].iloc[i+1]] += 1
+        transitions.loc[all_df['chord'].iloc[i],
+                        all_df['chord'].iloc[i+1]] += 1
 
     transitions = transitions.drop(['start_chord', 'end_chord'], axis=0)
     transitions = transitions.drop(['start_chord', 'end_chord'], axis=1)
